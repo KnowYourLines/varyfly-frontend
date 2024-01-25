e6686bdd10db6675800788f03b6be82deb9c6938
Johnny <johnkcli@hotmail.com>
2024-01-25 11:01:32 +0000

    can add date to flight leg

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Unstable_Grid2";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { v4 as uuidv4 } from "uuid";
 
 import FlightLeg from "./components/FlightLeg";
 
 export default function App() {
   const [flightLegs, setFlightLegs] = useState([
-    { from: null, to: null, id: uuidv4() },
-    { from: null, to: null, id: uuidv4() },
+    { from: null, to: null, date: null, id: uuidv4() },
+    { from: null, to: null, date: null, id: uuidv4() },
   ]);
   const addFlight = () => {
-    setFlightLegs([...flightLegs, { from: null, to: null, id: uuidv4() }]);
+    setFlightLegs([
+      ...flightLegs,
+      { from: null, to: null, date: null, id: uuidv4() },
+    ]);
   };
   const removeFlight = (id) => {
     setFlightLegs(flightLegs.filter((flightLeg) => flightLeg.id !== id));
   };
-  const handleFlightLegChange = (flightLegId, from, to) => {
+  const handleFlightLegChange = (flightLegId, from, to, date) => {
     setFlightLegs(
       flightLegs.map((flightLeg) => {
         if (flightLeg.id === flightLegId) {
-          return { ...flightLeg, from: from, to: to };
+          return { ...flightLeg, from: from, to: to, date: date.toISOString() };
         } else {
           return flightLeg;
         }
@@ -31,26 +36,31 @@ export default function App() {
   };
 
   return (
-    <Container maxWidth="100%">
-      <Box sx={{ my: "1%" }}>
-        <Grid container rowSpacing={2}>
-          {flightLegs.map((flightLeg, index) => (
-            <Grid item xs={12} key={flightLeg.id}>
-              <FlightLeg
-                flightLegId={flightLeg.id}
-                from={flightLeg.from}
-                to={flightLeg.to}
-                flightLegOrder={index}
-                onChange={handleFlightLegChange}
-                onRemove={removeFlight}
-              />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Container maxWidth="100%">
+        <Box sx={{ my: "1%" }}>
+          <Grid container rowSpacing={2}>
+            {flightLegs.map((flightLeg, index) => (
+              <Grid item xs={12} key={flightLeg.id}>
+                <FlightLeg
+                  flightLegId={flightLeg.id}
+                  from={flightLeg.from}
+                  to={flightLeg.to}
+                  date={flightLeg.date}
+                  flightLegOrder={index}
+                  onChange={handleFlightLegChange}
+                  onRemove={removeFlight}
+                />
+              </Grid>
+            ))}
+            <Grid item xs={12}>
+              <Button variant="contained" onClick={addFlight}>
+                Add Flight
+              </Button>
             </Grid>
-          ))}
-          <Grid item xs={12}>
-            <Button onClick={addFlight}>Add Flight</Button>
           </Grid>
-        </Grid>
-      </Box>
-    </Container>
+        </Box>
+      </Container>
+    </LocalizationProvider>
   );
 }
--- src/components/FlightLeg.js
+++ src/components/FlightLeg.js
@@ -1,32 +1,47 @@
 import CitySearch from "./CitySearch";
 import Grid from "@mui/material/Unstable_Grid2";
 import Button from "@mui/material/Button";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function FlightLeg({
   flightLegId,
   from,
   to,
+  date,
   flightLegOrder,
   onChange,
   onRemove,
 }) {
   const changeFrom = (event, newValue) => {
-    onChange(flightLegId, newValue, to);
+    onChange(flightLegId, newValue, to, date);
   };
   const changeTo = (event, newValue) => {
-    onChange(flightLegId, from, newValue);
+    onChange(flightLegId, from, newValue, date);
+  };
+  const changeDate = (newValue) => {
+    onChange(flightLegId, from, to, newValue);
   };
   return (
     <Grid container spacing={2}>
-      <Grid item xs={5.5}>
+      <Grid item xs={3.5}>
         <CitySearch inputLabel="From" handleChange={changeFrom} />
       </Grid>
-      <Grid item xs={5.5}>
+      <Grid item xs={3.5}>
         <CitySearch inputLabel="To" handleChange={changeTo} />
+      </Grid>
+      <Grid item xs={3.5}>
+        <DatePicker
+          value={date ? date : ""}
+          onChange={(newValue) => {
+            changeDate(newValue);
+          }}
+        />
       </Grid>
       {flightLegOrder > 1 && (
         <Grid item xs={1}>
-          <Button onClick={() => onRemove(flightLegId)}>Remove</Button>
+          <Button variant="contained" onClick={() => onRemove(flightLegId)}>
+            Remove
+          </Button>
         </Grid>
       )}
     </Grid>
