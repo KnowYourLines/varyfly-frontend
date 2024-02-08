ae1a582d5728aa975725ce8ec882c51da304fd69
Johnny <johnkcli@hotmail.com>
2024-02-08 03:26:33 +0000

    default page size is 10

--- src/components/DirectDestinations.js
+++ src/components/DirectDestinations.js
@@ -68,7 +68,7 @@ export default function DirectDestinations({ destinations, origin }) {
       }}
       initialState={{
         pagination: {
-          paginationModel: { page: 0, pageSize: 5 },
+          paginationModel: { page: 0, pageSize: 10 },
         },
       }}
       pageSizeOptions={[5, 10]}
